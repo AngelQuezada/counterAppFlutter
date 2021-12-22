714c5a67a2c2ce7e87487fbbdd347431724edb67
Miguel Angel Quezada Galvan <azzy.ralsie@gmail.com>
2021-12-22 10:27:00 -0600

    Commit inicial

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return const Scaffold(
+      backgroundColor: Colors.purple,
+      body: Center(
+        child: Text('Holi Jolty'),
+      ),
+    );
+  }
+}
